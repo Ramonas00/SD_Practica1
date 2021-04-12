1b883609bb381c46776d518d1097b54707d6d530
Evaldas <evaldas.r2000@gmail.com>
2021-04-12 21:52:19 +0200

    Config principal

---
+++ client.py
@@ -0,0 +1,14 @@
+import xmlrpc.client
+import sys
+
+proxy = xmlrpc.client.ServerProxy('http://localhost:9000')
+#print(proxy.list_contents(sys.argv[1]))
+if (sys.argv[1] == "worker"):
+    if (sys.argv[2] == "create"):
+        proxy.crear_worker()
+    if (sys.argv[2] == "delete"):
+        proxy.delete_worker(sys.argv[3])
+    if (sys.argv[2] == "list"):
+        proxy.list_worker()
+if (sys.argv[1] == "job"):
+    proxy.jobRun()
---
+++ server.py
@@ -0,0 +1,59 @@
+from xmlrpc.server import SimpleXMLRPCServer
+import logging
+import requests
+from multiprocessing import Process
+
+WORKERS = {}
+WORKER_ID = 0
+
+
+def start_worker(n):
+    #aux = requests.get("http://localhost:8000/Documents/SD/Pract-1/fitxer1.txt")
+    #print(aux.text)
+
+class ServerMethods:
+
+
+    def crear_worker(self):
+        global WORKERS
+        global WORKER_ID
+
+        proc = Process(target=start_worker, args=(WORKER_ID,))
+        proc.start()
+        WORKERS[WORKER_ID] = proc
+
+        WORKER_ID += 1
+
+
+    def delete_worker(self, num):
+        if not WORKERS[num]:
+            return -1
+        
+        WORKERS[num].join()
+
+
+    def list_worker(self):
+        return WORKERS
+
+
+    def list_contents(self, dir_name):
+        return dir_name
+
+
+if __name__ == '__main__':
+    # Set up logging
+    logging.basicConfig(level=logging.INFO)
+
+    server = SimpleXMLRPCServer(
+        ('localhost', 9000),
+        logRequests=True,
+    )
+
+    server.register_instance(ServerMethods())
+
+    # Start the server
+    try:
+        print('Use Control-C to exit')
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print('Exiting')
