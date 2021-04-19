d2a24859b87a613971fe887a87bacc70daa72d0a
Evaldas <evaldas.r2000@gmail.com>
2021-04-19 12:25:37 +0200

    Upgrades

--- client.py
+++ client.py
@@ -6,10 +6,11 @@ proxy = xmlrpc.client.ServerProxy('http://localhost:9000')
 #print(proxy.list_contents(sys.argv[1]))
 if (sys.argv[1] == "worker"):
     if (sys.argv[2] == "create"):
+        print("Hola")
         proxy.crear_worker()
     if (sys.argv[2] == "delete"):
         proxy.delete_worker(sys.argv[3])
     if (sys.argv[2] == "list"):
         proxy.list_worker()
 if (sys.argv[1] == "job"):
-    proxy.jobRun()
+    proxy.jobRun(sys.argv[2], sys.argv[3])
--- server.py
+++ server.py
@@ -1,16 +1,62 @@
 from xmlrpc.server import SimpleXMLRPCServer
 import logging
 import requests
+import simplejson as json
+import redis
+import string
 from multiprocessing import Process
 
 WORKERS = {}
 WORKER_ID = 0
+JOBID = 0
+
+REDIS_PORT = 6379
+REDIS_HOST = '127.0.0.1'
+
+r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT)
 
 
 def start_worker(n):
-    #aux = requests.get("http://localhost:8000/Documents/SD/Pract-1/fitxer1.txt")
-    #print(aux.text)
+    packed = r.blpop(['queue:email'], 2)
+    while True:
+        while packed is None:
+            asyncio.sleep(1)
+            packed = r.blpop(['queue:email'], 2)
+        print(packed)
 
+        to_send = json.loads(packed[1])
+        operacio = to_send["Operacio"]
+        line = requests.get(to_send["URL"]).text
+
+        if operacio == "wordcount":
+            wordCount = len(linespilt())
+        elif operacio == "countwords":
+            d = dict()
+
+            # Remove the leading spaces and newline character /// https://www.geeksforgeeks.org/python-count-occurrences-of-each-word-in-given-text-file-using-dictionary/
+            line = line.strip()
+  
+            # Convert the characters in line to 
+            # lowercase to avoid case mismatch
+            line = line.lower()
+            
+            # Remove the punctuation marks from the line
+            line = line.translate(line.maketrans("", "", string.punctuation))
+            
+            # Split the line into words
+            words = line.split(" ")
+            
+            # Iterate over each word in line
+            for word in words:
+                # Check if the word is already in dictionary
+                if word in d:
+                    # Increment count of word by 1
+                    d[word] = d[word] + 1
+                else:
+                    # Add the word to dictionary with count 1
+                    d[word] = 1
+    
+    return True
 
 class ServerMethods:
 
@@ -24,6 +70,7 @@ class ServerMethods:
         WORKERS[WORKER_ID] = proc
 
         WORKER_ID += 1
+        return True         # Limitació del xmlrpc
 
 
     def delete_worker(self, num):
@@ -31,15 +78,30 @@ class ServerMethods:
             return -1
         
         WORKERS[num].join()
+        return True
 
 
     def list_worker(self):
         return WORKERS
 
 
-    def list_contents(self, dir_name):
-        return dir_name
+    def jobRun(opcio, urls):
+        global JOBID
 
+        opcio = opcio[4:]
+        urls = urls.split(",")
+        for x in range(len(urls)):
+            url = urls[x]
+            data = {
+                'JOBID': JOBID,
+                'Operacio': opcio,
+                'URL': url
+            }
+            # ENVIAR A REDIS (JOBID, opcio, url)
+            r.rpush('queue:email', json.dumps(data))
+
+        JOBID += 1
+        return True
 
 if __name__ == '__main__':
     # Set up logging
