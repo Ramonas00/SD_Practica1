7e5c7bb746df9dbdd20c2f7332f52807096a3f35
Evaldas <evaldas.r2000@gmail.com>
2021-04-23 16:34:11 +0200

    +Requeriments i bonico

--- client.py
+++ client.py
@@ -3,10 +3,8 @@ import sys
 
 
 proxy = xmlrpc.client.ServerProxy('http://localhost:9000')
-#print(proxy.list_contents(sys.argv[1]))
 if (sys.argv[1] == "worker"):
     if (sys.argv[2] == "create"):
-        print("Hola")
         proxy.crear_worker()
     if (sys.argv[2] == "delete"):
         proxy.delete_worker(sys.argv[3])
